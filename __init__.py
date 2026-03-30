e9be32205bb7539a4c617b98ed80970f5a4f1066
anwitabandaru-gif <anwita.bandaru@gmail.com>
2026-03-30 12:29:50 -0700

    try smth?

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from flask_login import LoginManager
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
+import re
+
 
 
 # Load environment variables from .env file
@@ -44,6 +46,16 @@ allowed_origins = [
     'https://acs.opencodingsociety.com',
 ]
 
+# Add this after your CORS setup in __init__.py
+@app.after_request
+def add_cors_headers(response):
+    origin = request.headers.get('Origin')
+    if origin in allowed_origins:
+        response.headers['Access-Control-Allow-Origin'] = origin
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
 
 # Admin Defaults
 app.config['ADMIN_USER'] = os.environ.get('ADMIN_USER') or 'Admin Name'
