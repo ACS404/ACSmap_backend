d6bb163c267972693007d7050c42d85951ab9c4b
anwitabandaru-gif <anwita.bandaru@gmail.com>
2026-03-31 16:23:45 -0700

    add changes

--- __init__.py
+++ __init__.py
@@ -46,16 +46,6 @@ allowed_origins = [
     'https://acs.opencodingsociety.com',
 ]
 
-# Add this after your CORS setup in __init__.py
-@app.after_request
-def add_cors_headers(response):
-    origin = request.headers.get('Origin')
-    if origin in allowed_origins:
-        response.headers['Access-Control-Allow-Origin'] = origin
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Origin'
-    return response
 
 # Admin Defaults
 app.config['ADMIN_USER'] = os.environ.get('ADMIN_USER') or 'Admin Name'
